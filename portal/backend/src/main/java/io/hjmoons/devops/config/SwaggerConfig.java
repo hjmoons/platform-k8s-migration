2a373d0c39f242df64d900d6d7ab084ed85c49a1
HyoJong Moon <31604340+hjmoons@users.noreply.github.com>
2026-01-23 16:37:39 +0900

    feature(backend): Keycloak OAuth 인증 연동 (#1)

--- portal/backend/src/main/java/io/hjmoons/devops/config/SwaggerConfig.java
+++ portal/backend/src/main/java/io/hjmoons/devops/config/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package io.hjmoons.devops.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +17,14 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String issuerUri;
+
     @Bean
     public OpenAPI openAPI() {
+        String authUrl = issuerUri + "/protocol/openid-connect/auth";
+        String tokenUrl = issuerUri + "/protocol/openid-connect/token";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("DevOps Platform API")
@@ -20,6 +32,18 @@ public class SwaggerConfig {
                         .version("v1.0.0"))
                 .servers(List.of(
                         new Server().url("/api").description("Default Server")
-                ));
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("keycloak", new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .flows(new OAuthFlows()
+                                        .authorizationCode(new OAuthFlow()
+                                                .authorizationUrl(authUrl)
+                                                .tokenUrl(tokenUrl)
+                                        )
+                                )
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("keycloak"));
     }
 }
